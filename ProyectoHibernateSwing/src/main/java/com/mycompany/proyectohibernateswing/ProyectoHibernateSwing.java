99b8de32a1c0e75cec5f110250310327185a4e90
Jon-Maneiro <32477643+Jon-Maneiro@users.noreply.github.com>
2022-11-21 20:38:28 +0100

    Creacion del proyecto 2
    antes lo cree con InteliJ, he borrado ese y he creado uno con NetBeans

---
+++ ProyectoHibernateSwing/src/main/java/com/mycompany/proyectohibernateswing/ProyectoHibernateSwing.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.proyectohibernateswing;
+
+import ui.Menu;
+
+/**
+ *
+ * @author Jon Maneiro García
+ */
+public class ProyectoHibernateSwing {
+
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.setVisible(true);
+    }
+}
